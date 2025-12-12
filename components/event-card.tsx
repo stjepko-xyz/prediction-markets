787372fd492367d6299cc63ec0163369dbcfc3a4
stjepko-xyz <24295554+stjepko-xyz@users.noreply.github.com>
2025-12-12 21:22:35 +0100

    Add yes/no order-buttons styling based on 1 or more markets

--- components/event-card.tsx
+++ components/event-card.tsx
@@ -37,81 +37,124 @@ const EventCard = ({ id, title, image, volume, markets }: EventCardProps) => {
   const { setSide, setMarket, market } = useOrder();
 
   return (
-    <Card className="p-4 gap-4 w-full justify-between">
-      {(title || image) && (
-        <div className="flex items-start justify-between gap-4">
-          {title && (
-            <Link href={`/event/${id}`} className="hover:underline">
-              <h5 className="line-clamp-2 font-semibold">{title}</h5>
-            </Link>
-          )}
-          <div className="flex h-[40px] w-[40px]">
-            {image && !imageError && (
-              <Image
-                src={image}
-                alt={title}
-                width={40}
-                height={40}
-                className="rounded-md object-cover shrink-0"
-                onError={() => setImageError(true)}
-              />
-            )}
-          </div>
-        </div>
-      )}
-      {markets && (
-        <div className="space-y-2">
-          {markets.slice(0, 2).map((market, index) => {
-            const marketData = {
-              ticker: market?.ticker ?? `${id}-market-${index}`,
-              yesAsk: market?.yesAsk ?? 0,
-              yesBid: market?.yesBid ?? 0,
-              yesSubTitle: market?.yesSubTitle ?? "",
-              noAsk: market?.noAsk ?? 0,
-              noBid: market?.noBid ?? 0,
-              noSubTitle: market?.noSubTitle ?? "",
-            };
-            return (
-              <div
-                key={index}
-                className="flex items-center justify-between gap-4"
-              >
-                <span className="line-clamp-1">{market?.yesSubTitle}</span>
-                <div className="flex items-center gap-2">
-                  <p className="font-semibold">{toPercent(market?.yesAsk)}</p>
-                  <div className="flex">
-                    <ButtonGroup>
-                      <Button
-                        onClick={() => {
-                          setSide("yes");
-                          setMarket(marketData);
-                          setOrderModalOpen(true);
-                        }}
-                        variant={"outline"}
-                        size="sm"
-                        className="bg-green-100 text-green-700"
-                      >
-                        Yes
-                      </Button>
-
-                      <Button
-                        onClick={() => {
-                          setSide("no");
-                          setMarket(marketData);
-                          setOrderModalOpen(true);
-                        }}
-                        variant={"outline"}
-                        size="sm"
-                        className="bg-red-100 text-red-700"
-                      >
-                        No
-                      </Button>
-                    </ButtonGroup>
-                  </div>
-                </div>
+    <Card className="flex p-4 gap-4 w-full justify-between">
+      {(title || image || markets) && (
+        <div className="flex flex-col flex-1 space-y-4">
+          {(title || image) && (
+            <div className="flex items-start justify-between gap-4 mb-4">
+              {title && (
+                <Link href={`/event/${id}`} className="hover:underline">
+                  <h5 className="line-clamp-2 font-semibold">{title}</h5>
+                </Link>
+              )}
+              <div className="flex h-[40px] w-[40px]">
+                {image && !imageError && (
+                  <Image
+                    src={image}
+                    alt={title}
+                    width={40}
+                    height={40}
+                    className="rounded-md object-cover shrink-0"
+                    onError={() => setImageError(true)}
+                  />
+                )}
               </div>
-            );
-          })}
+            </div>
+          )}
+          {markets && (
+            <div className="flex flex-col flex-1 space-y-2">
+              {markets.slice(0, 2).map((market, index) => {
+                const marketData = {
+                  ticker: market?.ticker ?? `${id}-market-${index}`,
+                  yesAsk: market?.yesAsk ?? 0,
+                  yesBid: market?.yesBid ?? 0,
+                  yesSubTitle: market?.yesSubTitle ?? "",
+                  noAsk: market?.noAsk ?? 0,
+                  noBid: market?.noBid ?? 0,
+                  noSubTitle: market?.noSubTitle ?? "",
+                };
+
+                if (markets?.length === 1) {
+                  return (
+                    <div key={index} className="w-full flex flex-1">
+                      <ButtonGroup className="flex flex-1 w-full h-full">
+                        <Button
+                          onClick={() => {
+                            setSide("yes");
+                            setMarket(marketData);
+                            setOrderModalOpen(true);
+                          }}
+                          variant={"outline"}
+                          size="sm"
+                          className="flex-1 h-full text-md bg-green-100 text-green-700"
+                        >
+                          Yes {toPercent(market?.yesAsk)}
+                        </Button>
+
+                        <Button
+                          onClick={() => {
+                            setSide("no");
+                            setMarket(marketData);
+                            setOrderModalOpen(true);
+                          }}
+                          variant={"outline"}
+                          size="sm"
+                          className="flex-1 h-full text-md bg-red-100 text-red-700"
+                        >
+                          No {toPercent(market?.noAsk)}
+                        </Button>
+                      </ButtonGroup>
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div
+                      key={index}
+                      className="flex items-center justify-between gap-4"
+                    >
+                      <span className="line-clamp-1">
+                        {market?.yesSubTitle}
+                      </span>
+                      <div className="flex items-center gap-2">
+                        <p className="font-semibold">
+                          {toPercent(market?.yesAsk)}
+                        </p>
+                        <div className="flex">
+                          <ButtonGroup>
+                            <Button
+                              onClick={() => {
+                                setSide("yes");
+                                setMarket(marketData);
+                                setOrderModalOpen(true);
+                              }}
+                              variant={"outline"}
+                              size="sm"
+                              className="bg-green-100 text-green-700"
+                            >
+                              Yes
+                            </Button>
+
+                            <Button
+                              onClick={() => {
+                                setSide("no");
+                                setMarket(marketData);
+                                setOrderModalOpen(true);
+                              }}
+                              variant={"outline"}
+                              size="sm"
+                              className="bg-red-100 text-red-700"
+                            >
+                              No
+                            </Button>
+                          </ButtonGroup>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                }
+              })}
+            </div>
+          )}
         </div>
       )}
       {volume && (
